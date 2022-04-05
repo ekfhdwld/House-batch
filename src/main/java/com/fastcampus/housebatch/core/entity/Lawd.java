e1e7b110b8326ed6ff1498901db8cbc6b9a1d125
jihye <mickey0426@naver.com>
2022-04-05 12:05:01 +0900

    add Entity
    - Lawd Entity 구현

---
+++ src/main/java/com/fastcampus/housebatch/core/entity/Lawd.java
@@ -0,0 +1,41 @@
+package com.fastcampus.housebatch.core.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@EntityListeners(EntityListeners.class)
+@NoArgsConstructor
+@Table(name = "lawd")
+public class Lawd {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long lawdId;
+	
+	
+	@Column(nullable = false)
+	private String lawdCd;
+	
+	@Column(nullable = false)
+	private String lawdDong;
+	
+	@Column(nullable = false)
+	private Boolean exist;
+	
+	@CreatedDate
+	private LocalDateTime createdAt;
+	
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
+}
