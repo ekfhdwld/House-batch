e4d1484404c4d101299cfeadc84fe0903a556d4d
jihye <mickey0426@naver.com>
2022-04-11 10:38:49 +0900

    add GuLawdTasklet
    - ExecutionContext에 저장할 데이터를 처리할 tasklet 클래스 생성
    - AptDealInserJobConfig에 적용
    - JobParameterValidator Refactor

--- src/main/java/com/fastcampus/housebatch/job/apt/AptDealInsertJobConfig.java
+++ src/main/java/com/fastcampus/housebatch/job/apt/AptDealInsertJobConfig.java
@@ -3,11 +3,12 @@ package com.fastcampus.housebatch.job.apt;
 import com.fastcampus.housebatch.adapter.ApartmentApiResource;
 import com.fastcampus.housebatch.core.dto.AptDealDto;
 import com.fastcampus.housebatch.core.repository.LawdRepository;
-import com.fastcampus.housebatch.job.validator.LawdCdParameterValidator;
 import com.fastcampus.housebatch.job.validator.YearMonthParameterValidator;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.*;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersValidator;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
@@ -15,7 +16,6 @@ import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.CompositeJobParametersValidator;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.xml.StaxEventItemReader;
 import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
@@ -27,7 +27,6 @@ import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 import java.time.YearMonth;
 import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @AllArgsConstructor
@@ -38,7 +37,6 @@ public class AptDealInsertJobConfig {
 	private final StepBuilderFactory stepBuilderFactory;
 	
 	private final ApartmentApiResource apartmentApiResource;
-	private final LawdRepository lawdRepository;
 	
 	@Bean
 	public Job aptDealInsertJob(
@@ -48,21 +46,13 @@ public class AptDealInsertJobConfig {
 	) {
 		return jobBuilderFactory.get("aptDealInsertJob")
 		  .incrementer(new RunIdIncrementer())
-		  .validator(aptDealJobParameterValidator())
+		  .validator(new YearMonthParameterValidator())
 		  .start(guLawdCdStep)
 		  .on("CONTINUABLE").to(contextPrintStep).next(guLawdCdStep)
 		  .from(guLawdCdStep)
 		  .on("*").end()
 		  .end()
 		  .build();
-	}
-	
-	private JobParametersValidator aptDealJobParameterValidator() {
-		CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
-		validator.setValidators(Arrays.asList(
-		  new YearMonthParameterValidator()
-		));
-		return validator;
 	}
 	
 	@JobScope
@@ -73,46 +63,10 @@ public class AptDealInsertJobConfig {
 		  .build();
 	}
 	
-	/**
-	 * ExecutionContext에 저장할 데이터
-	 * 1. guLawdCD - 구 코드 -> 다음 스텝에서 활용할 값
-	 * 2. guLawdCdList - 구 코드 리스트
-	 * 3. itemCount - 남아있는 구 코드의 갯수
-	 */
 	@StepScope
 	@Bean
-	public Tasklet guLawdCdTasklet() {
-		return (contribution, chunkContext) -> {
-			StepExecution stepExecution = chunkContext.getStepContext().getStepExecution();
-			ExecutionContext executionContext = stepExecution.getJobExecution().getExecutionContext();
-			
-			//데이터가 있으면 다음 스텝을 실행하도록 하고, 데이터가 없으면 종료되도록 한다
-			//데이터가 있으면 -> CONTINUABLE
-			List<String> guLawdCdList;
-			if(!executionContext.containsKey("guLawdCdList")){
-				guLawdCdList = lawdRepository.findDistinctGuLawdCd();
-				executionContext.put("guLawdCdList", guLawdCdList);
-				executionContext.putInt("itemCount", guLawdCdList.size());
-			}else{
-				guLawdCdList = (List<String>) executionContext.get("guLawdCdList");
-			}
-			
-			Integer itemCount = executionContext.getInt("itemCount");
-			
-			if(itemCount == 0){
-				contribution.setExitStatus(ExitStatus.COMPLETED);
-				return RepeatStatus.FINISHED;
-			}
-			
-			itemCount--;
-			
-			String guLawdCd = guLawdCdList.get(itemCount);
-			executionContext.putString("guLawdCd", guLawdCd);
-			executionContext.putInt("itemCount", itemCount);
-			
-			contribution.setExitStatus(new ExitStatus("CONTINUABLE"));
-			return RepeatStatus.FINISHED;
-		};
+	public Tasklet guLawdCdTasklet(LawdRepository lawdRepository) {
+		return new GuLawdTasklet(lawdRepository);
 	}
 	
 	@JobScope
