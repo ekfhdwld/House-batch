f87e5f35337637ef8bdc5243e5160d3699ecb61c
jihye <mickey0426@naver.com>
2022-04-12 16:28:42 +0900

    add @Query
    - N+1문제 해결을 위해 쿼리문 추가

--- src/main/java/com/fastcampus/housebatch/core/repository/AptDealRepository.java
+++ src/main/java/com/fastcampus/housebatch/core/repository/AptDealRepository.java
@@ -3,6 +3,7 @@ package com.fastcampus.housebatch.core.repository;
 import com.fastcampus.housebatch.core.entity.Apt;
 import com.fastcampus.housebatch.core.entity.AptDeal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -13,5 +14,6 @@ public interface AptDealRepository extends JpaRepository<AptDeal, Long> {
 	Optional<AptDeal> findAptDealByAptAndExclusiveAreaAndDealDateAndDealAmountAndFloor(
 	  Apt apt, Double exclusiveArea, LocalDate dealDate, Long dealAmount, Integer floor);
 	
+	@Query("select ad from AptDeal ad join fetch ad.apt where ad.dealCanceled = 0 and ad.dealDate = ?1")
 	List<AptDeal> findByDealCanceledIsFalseAndDealDateEquals(LocalDate dealDate);
 }
