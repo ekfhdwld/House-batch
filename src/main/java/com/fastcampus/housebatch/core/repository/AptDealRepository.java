f136bec843350f8a11be577799c0b0559bb4fc3c
jihye <mickey0426@naver.com>
2022-04-12 16:24:37 +0900

    add AptDealRepository method

--- src/main/java/com/fastcampus/housebatch/core/repository/AptDealRepository.java
+++ src/main/java/com/fastcampus/housebatch/core/repository/AptDealRepository.java
@@ -5,10 +5,13 @@ import com.fastcampus.housebatch.core.entity.AptDeal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface AptDealRepository extends JpaRepository<AptDeal, Long> {
 	
 	Optional<AptDeal> findAptDealByAptAndExclusiveAreaAndDealDateAndDealAmountAndFloor(
 	  Apt apt, Double exclusiveArea, LocalDate dealDate, Long dealAmount, Integer floor);
+	
+	List<AptDeal> findByDealCanceledIsFalseAndDealDateEquals(LocalDate dealDate);
 }
