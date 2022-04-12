0411355ee37e70906e688c94a795512187cdec31
jihye <mickey0426@naver.com>
2022-04-12 11:21:40 +0900

    add jibun getter

--- src/main/java/com/fastcampus/housebatch/core/dto/AptDealDto.java
+++ src/main/java/com/fastcampus/housebatch/core/dto/AptDealDto.java
@@ -9,6 +9,7 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 /**
  * 아파트 실거래가 api의 각각의 거래 정보를 담는 객체
@@ -49,6 +50,10 @@ public class AptDealDto {
 	
 	@XmlElement(name = "지번")
 	private String jibun;
+	
+	public String getJibun(){
+		return Optional.ofNullable(jibun).orElse("");
+	}
 	
 	@XmlElement(name = "지역코드")
 	private String regionalCode;
